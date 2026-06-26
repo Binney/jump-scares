c2bb791ab36640b8cc7c4f1dfa609edac8708f09
Sarah Binney <Sarah.Binney@softwire.com>
2026-06-26 21:39:22 +0100

    footer styling thanks to dani

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,12 @@ import InlineLink from "./InlineLink";
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-200 p-4 mt-8">
-            <p className="text-gray-600">
+        <footer className="bg-gray-200 p-4 mt-8 flex gap-4">
+            <span className="text-gray-600">
                 jumpscares
-            </p>
-            <InlineLink href="/#about">About</InlineLink>
+            </span>
+            |
+            <InlineLink href="/#about">About</InlineLink> |
             <InlineLink href="/#contact">Contact</InlineLink>
         </footer>
     );
